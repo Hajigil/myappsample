0bfa0c1055e56cfc5259194d715f1b6d7bd8809f
Shaheen Hajigil <hajigil@gmail.com>
2018-03-17 22:24:35 -0400

    Initial commit

---
+++ sand.py
@@ -0,0 +1,13 @@
+"""sand.py, by Shaheen Hajigil, 03-15-2018
+This program has the user guess the current opprtunities and threats
+"""
+
+def main():
+    print ("Hit the Counter button as many times as you want.")
+# test change git
+
+def evenOdd1 = You hit the counter an odd number of times
+def evenOdd2 = You hit the counter an even number of times
+   # if Hit Counter was hit odd number of times program must print evenOdd1
+    print(evenOdd1)
+    print(evenOdd2) 
